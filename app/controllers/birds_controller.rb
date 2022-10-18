68212ed7fa35a019ded0003ad04ca7c8be6dab6f
Michael Okoe <michael.okoejr@gmail.com>
2022-10-18 01:36:01 +0000

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,16 +1,21 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
+# POST /birds
   def create
-    bird = Bird.create(bird_params)
+# create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+# rescue ActiveRecord::RecordInvalid => invalid
+# render_unprocessable_entity_response(invalid)
   end
 
   # GET /birds/:id
@@ -19,11 +24,14 @@ class BirdsController < ApplicationController
     render json: bird
   end
 
-  # PATCH /birds/:id
+# PATCH /birds/:id
   def update
+# update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+# rescue ActiveRecord::RecordInvalid => invalid
+# render_unprocessable_entity_response(invalid)
   end
 
   # DELETE /birds/:id
@@ -45,6 +53,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
